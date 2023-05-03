41d274671bc89d6ab7bd7974549fae77e4145e1b
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-05-03 08:54:18 -0400

    added PolarBear

---
+++ PolarBear.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PolarBear here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PolarBear extends Counter
+{
+    public void act()
+    {
+        PolarBear polarbear = new PolarBear();
+        setObject = 
+    }
+}
