3c7bce463541e57f53c50e12b5465dba93fad9c0
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-05-19 20:27:34 -0400

    polar bear can move left and right

--- PolarBear.java
+++ PolarBear.java
@@ -14,6 +14,11 @@ public class PolarBear extends Actor
      */
     public void act()
     {
-        move(1);
+        if(Greenfoot.isKeyDown("left")) {
+            move(-1);
+        }
+        else if (Greenfoot.isKeyDown("right")) {
+            move(1);
+        }
     }
 }
