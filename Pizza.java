4ceaa0152e5a5e794bff234ca6d632f5915bacac
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-05-19 20:25:24 -0400

    pizza moving down by 1 unit every second

--- Pizza.java
+++ Pizza.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Pizza here.
+ * Food for the polar bear to eat
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Brian Wang
+ * @version May 2023
  */
 public class Pizza extends Actor
 {
@@ -14,6 +14,6 @@ public class Pizza extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        setLocation(getX(), getY() + 1);
     }
 }
