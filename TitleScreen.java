dab2f130c0294c7d82094f79116a66bbe11b3b8e
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-05-21 00:08:43 -0400

    change a bit of formatting

--- TitleScreen.java
+++ TitleScreen.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class TitleScreen extends World
 {
     Label titleLable = new Label("Hungry Polar Bear", 75);
-
+    
     /**
      * Constructor for objects of class TitleScreen.
      * 
