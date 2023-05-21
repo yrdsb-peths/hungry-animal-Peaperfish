07e0fa5c3f765fe5858fa7c8190fc92d0f733c11
Brain Wang <82687988+Peaperfish@users.noreply.github.com>
2023-05-20 23:12:27 -0400

    added bg image, pizza, and polar bear to title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class TitleScreen extends World
 {
     Label titleLable = new Label("Hungry Polar Bear", 75);
-    
+
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -19,20 +19,39 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 500, 1); 
         
+        // bg image
+        GreenfootImage bg = new GreenfootImage("background.jpg");
+        bg.scale(getWidth(), getHeight());
+        setBackground(bg);
+        
+        // title size
         addObject(titleLable, getWidth()/2, getHeight()/2);
+        prepare();
     }
-    
+
     /**
      * the main world act loop
      */
-    
+
     public void act() {
         // starts the game if player presses the space bar
         if(Greenfoot.isKeyDown("space")) {
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
-        
+
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        PolarBear polarBear = new PolarBear();
+        addObject(polarBear,511,139);
+        Pizza pizza = new Pizza();
+        addObject(pizza,185,139);
     }
 }
 
